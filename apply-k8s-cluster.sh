22c288e184855cda227010e252c020e067d21f89
darivs <dtack192@gmail.com>
2019-10-23 13:13:04 +0200

    Remove postgres authentication

--- apply-k8s-cluster.sh
+++ apply-k8s-cluster.sh
@@ -4,6 +4,6 @@ kubectl create secret -n messaging generic vernemq-certifications \
                                         --from-file=./certificate/server.csr \
                                         --from-file=./certificate/server.crt \
                                         --from-file=./certificate/server.key
-
-kubectl apply -f k8s/postgres.yaml
+kubectl create secret -n messaging generic vernemq-passwd \
+					--from-file=./vmq.passwd
 kubectl apply -f k8s/vernemq-cluster.yaml
